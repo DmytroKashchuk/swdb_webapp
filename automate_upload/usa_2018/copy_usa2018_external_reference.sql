efbca7c858c84a0d9f2d45ab8485e5858ed9efd6
Dmytro Kashchuk <31933655+DmytroKashchuk@users.noreply.github.com>
2025-11-14 17:36:21 -0600

    Loading files

--- automate_upload/usa_2018/copy_usa2018_external_reference.sql
+++ automate_upload/usa_2018/copy_usa2018_external_reference.sql
@@ -1,3 +1,3 @@
 -- load data for: /home/dima/swdb/swdb_all_data/swdb_all_data_unziped/USA_2018/ExternalReference.TXT
 
-\copy usa_2018_external_reference FROM '/home/dima/swdb/swdb_all_data/swdb_all_data_unziped/USA_2018/ExternalReference_utf8.TXT' WITH (FORMAT csv, HEADER true, DELIMITER ',');
+\copy usa_2018_external_reference FROM '/home/dima/swdb/swdb_all_data/swdb_all_data_unziped/USA_aod_2018/ExternalReference_utf8.TXT' WITH (FORMAT csv, HEADER true, DELIMITER ',');
