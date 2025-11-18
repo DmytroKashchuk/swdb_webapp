7fada6e1ddac56ca8cb4ae76d92a25092357c8f0
Dmytro Kashchuk <31933655+DmytroKashchuk@users.noreply.github.com>
2025-11-18 10:16:24 -0600

    Loading files

--- flask_app/app.py
+++ flask_app/app.py
@@ -473,4 +473,4 @@ def compare_technologies():
 
 if __name__ == "__main__":
 	# ip to run the app: 127.0.0.1 and 10.20.5.20 on port 80 both
-	app.run(host="0.0.0.0", port=80, debug=True)
+	app.run(host="0.0.0.0", port=80)
